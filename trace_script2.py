f08851620094434d5da46c44c22fb3ee9517fa56
tmapua <tamimapua@gmail.com>
2020-06-27 13:14:17 +0800

    changed ip

--- trace_script2.py
+++ trace_script2.py
@@ -36,7 +36,7 @@ async def weather():
     #     await websocket.send(message)
     #     print(message)
     while True:
-        ip_address = "13.229.91.111"
+        ip_address = "3.136.83.123"
         async with websockets.connect("ws://{}:8080/weather".format(ip_address)) as websocket:
             while True:
                 message = await websocket.recv()
